31777e3b9fc6540b0a3487c915721dfe54cf8999
Oscar Ravelo <oscar91ravelo@gmail.com>
2020-09-28 22:25:41 +0200

    react exercise api

---
+++ src/Components/Rank/Rank.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Rank = () => {
+    return (
+        <div>
+        <div className="white f3" >{'current rank'}
+        <p>{'1#'}</p>
+        </div>
+
+        </div>
+    )
+}
+
+export default Rank;
